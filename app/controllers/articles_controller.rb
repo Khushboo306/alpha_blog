afc5ace47a3a4596334d920fac5fc7e8f2ea6412
Rails Udemy Course <railsudemycourse@gmail.com>
2019-11-22 16:45:36 +0530

    for deploying

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def index #to list articles route is localhost:3000/article
       @articles=Article.all
     end
@@ -19,18 +20,18 @@ class ArticlesController < ApplicationController
     end
     
     def show
-      @article = Article.find(params[:id])
+      
     end
     
    #  Route for editing articles takes the form ->
    #  /articles/:id/edit
 
     def edit 
-     @article = Article.find(params[:id])
+
     end
 
     def update
-      @article =Article.find(params[:id])
+     
 
       if @article.update(article_params)
          flash[:notice]="Articl was successfully updated"
@@ -39,8 +40,18 @@ class ArticlesController < ApplicationController
           render 'edit'
       end
     end
+    
+    def destroy
+    
+      @article.destroy
+      flash[:notice]="Article was deleted"
+      redirect_to articles_path
+    end
 
     private
+     def set_article
+      @article =Article.find(params[:id])
+     end
      def article_params
         params.require(:article).permit(:title, :description)
      end
